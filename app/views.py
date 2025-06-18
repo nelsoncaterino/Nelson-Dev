112106d17908e225e3b227fe0aa2066ca1c6a113
Nelson Caterino (nelsoncaterino) <nelsoncaterino@gmail.com>
2025-06-18 08:17:09 +0200

    my website

--- app/views.py
+++ app/views.py
@@ -10,15 +10,8 @@ def services(request):
     return render(request, 'services.html')
 
 def about(request):
-    context = {
-        'text': [{'content': 'lorem ipsum.'}],
-        'photo': [
-            {'image_url': 'img/1627469713006.jpeg', 'caption': 'Photo 1', 'z_index': 1},
-            {'image_url': 'img/capture_ecran_2025_02_26_091907.png', 'caption': 'Photo 2', 'z_index': 1},
-            {'image_url': 'img/IMG20240113134449.PNG', 'caption': 'Photo 3', 'z_index': 1},
-            {'image_url': 'img/raki.jpg', 'caption': 'Photo 4', 'z_index': 1},
-        ],
-    }
-    return render(request, 'about.html', context)
+    return render(request, 'about.html', )
+
 def contact(request):
     return render(request, 'contact.html')
+
