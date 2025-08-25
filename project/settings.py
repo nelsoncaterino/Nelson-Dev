1db4ee8851b547a5f33d7e1bf39b3d16beac221c
Nelson Caterino (nelsoncaterino) <nelsoncaterino@gmail.com>
2025-08-25 15:28:20 +0200

    mise à jours du site

--- project/settings.py
+++ project/settings.py
@@ -4,17 +4,6 @@ from decouple import config
 import os
 import socket
 from datetime import datetime
-# from .secrets import EMAIL_HOST_PASSWORD
-
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'info@nelson-dev.com'
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_SSL = False
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
